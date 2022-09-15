33f60103e200b0fceedcf9711361cfea71a8d2a6
Chad Eliason <cmestuff@gmail.com>
2022-09-14 20:02:39 -0500

    Update synteny_plots.sh

--- synteny_plots.sh
+++ synteny_plots.sh
@@ -1,20 +1,3 @@
-cd ~/uce-alcedinidae
-
-# head data/galGal3/galGal3_all.fa
-
-# Convert psl to sam-
-# https://github.com/lh3/samtools-legacy/blob/master/misc/psl2sam.pl
-# psl2sam.pl todChl.psl > todChl-to-galGal3.sam
-
-bedtools makewindows -i src -g cnee/galGal3.chr_length.txt -w 1000000 > cnee/test.bed
-head cnee/test.bed
-
-liftOver -multiple cnee/test.bed cnee/galGal3-to-todChl.over.chain todChlJupiter-agp.bed unMapped
-head todChlJupiter-agp.bed
-
-# ./JupiterPlot/jupiter name='todChl-to-galGal3' ref=data/galGal3/galGal3_all.fa fa=genomes/todChl/todChl.fasta
-
-
 # Chicken
 ref=data/galGal3/galGal3_all.fa
 ./JupiterPlot/jupiter2 name=todChl-to-galGal3 ref=$ref fa=genomes/todChl/todChl.fasta
@@ -30,4 +13,3 @@ wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/819/595/GCA_009819595.1_bM
 gunzip GCA_009819595.1_bMerNub1.pri_genomic.fna.gz
 ref=GCA_009819595.1_bMerNub1.pri_genomic.fna
 ./JupiterPlot/jupiter2 name=todChl-to-merNub ref=$ref fa=genomes/todChl/todChl.fasta
-
